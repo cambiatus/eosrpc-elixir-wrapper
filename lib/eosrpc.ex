83957af455d28abc3d81dd4141fa6a07c307c49b
Lucca <julienlucca@gmail.com>
2018-08-24 15:47:39 -0300

    Add macro to help on "bangnization" of functions

--- lib/eosrpc.ex
+++ lib/eosrpc.ex
@@ -10,8 +10,34 @@ defmodule EOSRPC do
   easy transaction push and account creation: `EOSRPC.Helper`
   """
 
+  @doc """
+  Macro that "bangify" functions, and normalize the exception throws when returning status tuples: `{:ok, value}` and `{:error, reason}`
 
+  ### Usage
+  ```
+  defmodule X do
+    import EOSRPC
 
+    def normal(param) do
+      if param == 1 do
+        {:ok, param + 1}
+      else
+        {:error, "not one"}
+      end
+    end
+
+    def normal!() do
+      unwrap_or_raise(normal())
+    end
+  end
+  ```
+  """
+  defmacro unwrap_or_raise(call) do
+    quote do
+      case unquote(call) do
+        {:ok, value} -> value
+        {:error, env} -> raise EOSRPC.Error, reason: env.body, url: env.url
+      end
     end
   end
 end
