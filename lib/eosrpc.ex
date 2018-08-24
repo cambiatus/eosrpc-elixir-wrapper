2ecfb4ec432d06e8acc0369f7b5f6537af3178fb
Lucca <julienlucca@gmail.com>
2018-08-24 15:47:20 -0300

    Remove old ways of doing post and get requests

--- lib/eosrpc.ex
+++ lib/eosrpc.ex
@@ -10,35 +10,8 @@ defmodule EOSRPC do
   easy transaction push and account creation: `EOSRPC.Helper`
   """
 
-  use Tesla
 
-  plug(Tesla.Middleware.JSON)
 
-  def get_request(url) do
-    url
-    |> get!()
-    |> validate_request()
-  end
-
-  def post_request(url, raw_data, quotify) do
-    data = if quotify, do: quotify(raw_data), else: raw_data
-
-    url
-    |> post!(data)
-    |> validate_request()
-  end
-
-  def post_request(url, raw_data) do
-    post_request(url, raw_data, false)
-  end
-
-  def validate_request(response) do
-    case response.status do
-      s when s in [200, 201, 202, 203, 204] -> {:ok, response.body}
-      _ -> {:error, response}
     end
   end
-
-  def quotify(nil), do: nil
-  def quotify(raw_data), do: ~s("#{raw_data}")
 end
