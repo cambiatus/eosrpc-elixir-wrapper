da7ad197778624171159eb36a31f59528b034bd5
Lucca <julienlucca@gmail.com>
2018-08-24 15:52:08 -0300

    Remove old way of doing post requests from account_history

--- lib/eosrpc/account_history.ex
+++ lib/eosrpc/account_history.ex
@@ -9,28 +9,28 @@ defmodule EOSRPC.AccountHistory do
   Get transaction data
   """
   def get_transaction(transaction_id) do
-    "/get_transaction" |> url() |> post_request(%{transaction_id: transaction_id})
+    "/get_transaction" |> url() |> post(%{transaction_id: transaction_id})
   end
 
   @doc """
   Get transactions for a given account
   """
   def get_transactions(account_name) do
-    "/get_transactions" |> url() |> post_request(%{account_name: account_name})
+    "/get_transactions" |> url() |> post(%{account_name: account_name})
   end
 
   @doc """
   Retrieve accounts associated with a public key
   """
   def get_key_accounts(public_key) do
-    "/get_key_accounts" |> url() |> post_request(%{public_key: public_key})
+    "/get_key_accounts" |> url() |> post(%{public_key: public_key})
   end
 
   @doc """
   Retrieve accounts which are created by the given account
   """
   def get_controlled_accounts(account_name) do
-    "/get_controlled_accounts" |> url() |> post_request(%{controlling_account: account_name})
+    "/get_controlled_accounts" |> url() |> post(%{controlling_account: account_name})
   end
 
   def url(url),
