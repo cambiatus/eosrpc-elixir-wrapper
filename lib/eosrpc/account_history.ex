07380be2c4886e6031d1cd240d25eda50bb1dc3c
Lucca <julienlucca@gmail.com>
2018-08-24 15:54:13 -0300

    Update bang variations to all functions on account_history.ex

--- lib/eosrpc/account_history.ex
+++ lib/eosrpc/account_history.ex
@@ -3,13 +3,22 @@ defmodule EOSRPC.AccountHistory do
   EOS Account History Apis Wrapper for Elixir
   """
 
+  use Tesla
+
   import EOSRPC
+
+  plug(Tesla.Middleware.JSON)
+  plug(EOSRPC.Middleware.Error)
 
   @doc """
   Get transaction data
   """
   def get_transaction(transaction_id) do
     "/get_transaction" |> url() |> post(%{transaction_id: transaction_id})
+  end
+
+  def get_transaction!(transaction_id) do
+    unwrap_or_raise(get_transaction(transaction_id))
   end
 
   @doc """
@@ -19,6 +28,10 @@ defmodule EOSRPC.AccountHistory do
     "/get_transactions" |> url() |> post(%{account_name: account_name})
   end
 
+  def get_transactions!(account_name) do
+    unwrap_or_raise(get_transactions(account_name))
+  end
+
   @doc """
   Retrieve accounts associated with a public key
   """
@@ -26,11 +39,21 @@ defmodule EOSRPC.AccountHistory do
     "/get_key_accounts" |> url() |> post(%{public_key: public_key})
   end
 
+  def get_key_accounts!(public_key) do
+    unwrap_or_raise(get_key_accounts(public_key))
+  end
+
   @doc """
   Retrieve accounts which are created by the given account
   """
   def get_controlled_accounts(account_name) do
-    "/get_controlled_accounts" |> url() |> post(%{controlling_account: account_name})
+    "/get_controlled_accounts"
+    |> url()
+    |> post(%{controlling_account: account_name})
+  end
+
+  def get_controlled_accounts!(account_name) do
+    unwrap_or_raise(get_controlled_accounts(account_name))
   end
 
   def url(url),
